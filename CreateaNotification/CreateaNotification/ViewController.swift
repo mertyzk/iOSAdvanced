9f6a2a2721d17b7cd49a4d6d963d41ea2a334f4d
RinKusChi <meyuzak@gmail.com>
2022-02-10 17:13:32 +0300

    Notifications Update

--- CreateaNotification/CreateaNotification/ViewController.swift
+++ CreateaNotification/CreateaNotification/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        UNUserNotificationCenter.current().delegate = self
+        
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge], completionHandler: {(granted,error) in
             
             self.permission = granted
@@ -26,6 +28,38 @@ class ViewController: UIViewController {
         })
     }
 
+    @IBAction func createNotificationButton(_ sender: Any) {
+        
+        if permission{
+            let content = UNMutableNotificationContent()
+            content.title = "Title"
+            content.subtitle = "Subtitle"
+            content.body = "Message"
+            content.badge = 1
+            content.sound = UNNotificationSound.default
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+            
+            let notificationRequest = UNNotificationRequest(identifier: "AnyId", content: content, trigger: trigger)
+            
+            UNUserNotificationCenter.current().add(notificationRequest, withCompletionHandler: nil)
+            
+            
+            
+        }
+        
+    }
+    
+}
 
+extension ViewController:UNUserNotificationCenterDelegate{
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.banner,.sound,.badge])
+        
+    }
+    
+    
 }
 
